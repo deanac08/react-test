387df6a53f9cd2529a4ed83c80826384e990dec4
deanac08 <deanac08@yahoo.com>
2022-01-17 11:26:39 -0800

    initial commit

---
+++ src/components/SomeClassComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class SomeClassComponent extends Component{
+
+    render(){
+        return <div>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </div>;
+    }
+}
+
+export default SomeClassComponent;
